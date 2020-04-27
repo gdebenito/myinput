81f2dd088109384bee5724bd234cda26240c63c9
Gonzalo De Benito <gdebenitocassado@gmail.com>
2020-04-27 11:41:40 +0200

    get rootpath from env

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
-rootPath = "/home/gon/docs/"
-inboxPath = rootPath + "be.org"
+import os
+
+try:
+    rootPath = os.getenv('MYINPUT_ROOT_PATH')
+    inboxPath = rootPath + "be.org"
+except:
+    print("MYINPUT_ROOT_PATH env not defined.")
+    print("Use: export MYINPUT_ROOT_PATH=/home/" + os.getenv('USER') +" in your .zshrc or .bashrc file.")
+    exit()
 prefixText= "- [ ] "
 sufixText= "\n"
